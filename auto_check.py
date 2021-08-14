b3f75a6716d9b27bacd89824be201a65f37e82d5
zz <2295789527@qq.com>
2021-08-14 11:10:18 +0800

    up

--- auto_check.py
+++ auto_check.py
@@ -7,13 +7,14 @@ with open("device_info.csv", "r", encoding="utf-8-sig") as f:
     # <序号>,<品牌>,<ip>,<账号>,<密码>
     device_info = f.read().split("\n")[1:]
 
-for info in device_info:
+for index, info in enumerate(device_info):
     device_type = info.split(",")[1]
     ipaddr =   info.split(",")[2]
     username = info.split(",")[3]
     password = info.split(",")[4]
+    out_info = "log for:" + ",".join([str(index+1), device_type, ipaddr, username])
     
-    print(str(datetime.now())+"connecting to device {}".format(ipaddr))
+    print(str(datetime.now())+" connecting to device {}".format(ipaddr))
     device_connect = {}
     device_connect = {
         'device_type': device_type,
@@ -26,26 +27,63 @@ for info in device_info:
     print(device_connect)
     conect = ConnectHandler(**device_connect)
     conect.enable()
-    _ = conect.send_command("dis clock")
-    print(_)
-    clock = _.split("\n")[0].replace(" bj","")
+
+    _clock = conect.send_command("dis clock")
+    print(_clock)
+    clock = _clock.split("\n")[0].replace(" bj","")
     print(clock)
+    if len(_clock.split("\n")) > 3:
+        print("请人工检查clock！")
     print("===============================================")
-    _ = conect.send_command("dis cpu")
-    print(_)
-    cpu = re.search("\d+%", _.split("\n")[3]).group()
+
+    _cpu = conect.send_command("dis cpu")
+    print(_cpu)
+    cpu = re.search("\d+%", _cpu.split("\n")[3]).group()
     print(cpu)
-    print("===============================================")
-    _ = conect.send_command("dis memory")
-    print(_)
-    mem = re.search("\d+\.?\d*%", _.split("\n")[3]).group()
-    print(mem)
+    if len(_cpu.split("\n")) > 5:
+        print("请人工检查cpu！")
     print("===============================================")
 
+    _memory = conect.send_command("dis memory")
+    print(_memory)
+    memory = re.search("(\d+\.?\d*)%", _memory.split("\n")[3]).group(1)
+    # 计算内存占用，保留两位小数
+    memory = str(round(100 - float(memory),2)) + "%"    
+    print(memory)
+    if len(_memory.split("\n")) > 7:
+        print("请人工检查memory！")
+    print("===============================================")
 
-    print(str(datetime.now()) + "saveing config from device {}".format(ipaddr))
-    #f = open("dev_"+ipaddr+"_cfg","w")
-    #f.write(running_config)
-    #f.close()
+    _power = conect.send_command("dis power")
+    print(_power)
+    power = re.findall("\d +([a-zA-Z]+)", _power)
+    print(power)
+    if len(_power.split("\n")) > 5:
+        print("请人工检查power！")
+    print("===============================================")
+
+    _fan = conect.send_command("dis fan")
+    print(_fan)
+    fan = re.findall("State    : ([a-zA-Z]+)", _fan)
+    print(fan)
+    if len(_fan.split("\n")) > 11:
+        print("请人工检查fan！")
+    print("===============================================")
+    
+    _environment = conect.send_command("dis environment")
+    print(_environment)
+    environment = re.findall(" [\d]     hotspot 1 (\d+)", _environment)
+    print(environment)
+    if len(_environment.split("\n")) > 7:
+        print("请人工检查fan！")
+    print("===============================================")
+
+    print(str(datetime.now()) + " saveing config from device {}".format(ipaddr))
+
+    out_logs = "\n".join(["\n\n"+"="*80, out_info, "time:"+str(datetime.now()), "<clock>\n"+_clock, 
+                        "<cpu>\n"+_cpu, "<memory>\n"+_memory, "<power>\n"+_power,
+                         "<fan>\n"+_fan, "<environment>\n"+_environment])
+    with open("logs.txt", "a+", encoding="utf-8") as f:
+        f.write(out_logs)
     print("===============================================")
 
--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,9 @@
 import re
 with open("dev_10.95.54.130_cfg", "r") as f:
     _ = f.read()
-    print(_.split("\n")[0].replace(" bj","") )
+    memory = re.search("(\d+\.?\d*)%", _.split("\n")[3]).group(1)
+    memory = str(round(100 - float(memory),2)) + "%"
+    print(memory)
 
 def get_info(message):
     pass
